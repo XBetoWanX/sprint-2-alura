b2e6449c0f24ef98fab3eca8536a676225c22797
XBetoWanX <salazaralberto277@gmail.com>
2022-05-20 13:22:44 -0600

    Creando la lógica incompleta para saber si ganaste o perdiste

--- sortear-palabra.js
+++ sortear-palabra.js
@@ -15,13 +15,15 @@ var aciertos = 0;
 var oportunidad = 6;
 var x = document.createElement("SPAN");
 var t;
+var letraRepetida = 0;
+
 //Funcion que da una palabra al azar
-function crearPalabraSecreta(){
-    
-    numeroRandom = Math.floor(Math.random()*(listaPalabras.length));
+function crearPalabraSecreta() {
+
+    numeroRandom = Math.floor(Math.random() * (listaPalabras.length));
     palabra = listaPalabras[numeroRandom];
     console.log(palabra);
-    for(var i = 0; i < palabra.length; i++){
+    for (var i = 0; i < palabra.length; i++) {
         t = document.createTextNode("___  ");
         x.appendChild(t);
         document.body.appendChild(x);
@@ -30,45 +32,69 @@ function crearPalabraSecreta(){
 
 
 const letras_teclado = document.querySelectorAll("#teclado button");
-for(var i = 0; i < letras_teclado.length; i++){
+for (var i = 0; i < letras_teclado.length; i++) {
     letras_teclado[i].addEventListener("click", capturarLetras);
 }
 
 //Funcion que captura las letras ingresadas por el usuario
-function capturarLetras(event){  
+function capturarLetras(event) {
     letra = event.target.textContent;
     compararLetras();
 
 }
 
 //Funcion que compara la palabra secreta con lo que el usuario digitó
-function compararLetras(){
-   if(palabra.includes(letra) == true){
-       console.log("Sí incluye");
-       if(repetida.includes(letra) == true){
-            console.log("repetida");
-       }else{
-         aciertos++;
-       }
-       if(aciertos == palabra.length){
-        alert("Ganaste!");
-        window.location.reload();
-    }
-   }else{
-        console.log("No incluye");
-        if(repetida.includes(letra) == true){
-            console.log("Repetida");
-        }else{
+// function compararLetras(){
+//    if(palabra.includes(letra) == true){
+//        console.log("Sí incluye");
+//        if(repetida.includes(letra) == true){
+//             console.log("repetida");
+//        }else{
+//          aciertos++;
+//        }
+//        if(aciertos == palabra.length){
+//         alert("Ganaste!");
+//         window.location.reload();
+//     }
+//    }else{
+//         console.log("No incluye");
+//         if(repetida.includes(letra) == true){
+//             console.log("Repetida");
+//         }else{
+//             oportunidad--;
+//         }
+//         if(oportunidad == 0){
+//             alert("GAME OVER!!")
+//             window.location.reload();
+//         }      
+//    }
+//    repetida.push(letra);
+//    letra = document.getElementById("palabra-secreta").value = "";
+// }
+
+function compararLetras() {
+    coincidencia = palabra.match(letra);
+    if (coincidencia == null) {
+        if(repetida.includes(letra) == false){
             oportunidad--;
+            console.log("No acertaste")
         }
-        if(oportunidad == 0){
-            alert("GAME OVER!!")
-            window.location.reload();
-        }      
-   }
-   repetida.push(letra);
-   letra = document.getElementById("palabra-secreta").value = "";
+        
+    }else{
+        if(repetida.includes(letra) == false){
+            aciertos++;
+            console.log(aciertos);
+        }
+    }
+    if(oportunidad == 0){
+        alert("Perdiste");
+    }
+    if(aciertos == palabra.length){
+        alert("Ganaste");
+    }
+    repetida.push(letra);
 }
+
 
 //Funcion que escribe la letra cada vez que aciertas una letra
 
